1b12003dc8ed16cb82d48a1bf15aa3bee1c162b5
Felipe Barcelos <felipebarcelospro@gmail.com>
2022-02-21 19:54:47 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+import Link from "next/link"
+
+export default function Home({ todos, pageTitle }) {
+  return (
+    <div>
+      <h1>{pageTitle}</h1>
+      
+      {todos.map(todo => (
+        <Link href={`/todos/${todo.id}`} key={todo.id} passHref>
+          <a style={{ display: "block" }}>{todo.title}</a>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/todos')
+  const todos = res.data
+
+  return {
+    props: {
+      todos,
+      pageTitle: "Todos"
+    }
+  }
+}
