fa099eeea17677af294831d5a235a99de2451af2
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2022-02-22 20:22:42 -0300

    feat: typescript

---
+++ pages/index.tsx
@@ -0,0 +1,82 @@
+import axios from "axios"
+import Link from "next/link"
+
+import { GetServerSideProps } from "next"
+import { useState } from "react"
+import { ITodo, ITodoResponse } from "./todos/[id]"
+
+type IHomePageProps = {
+  todos: ITodo[]
+  pageTitle: string
+}
+
+type IHandleCreateTodoDTO = {
+  title: string
+  completed: boolean
+}
+
+export default function Home({ todos, pageTitle }: IHomePageProps) {
+  const [todoList, setTodoList] = useState<ITodo[]>(todos)
+  const [titleInput, setTitleInput] = useState<string>('')
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const data = await handleCreateTodo({
+      title: titleInput,
+      completed: false
+    })
+
+    setTodoList([...todoList, data])
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleInput(e.target.value)
+  }
+
+  const handleCreateTodo = async (data: IHandleCreateTodoDTO): Promise<ITodo> => {
+    const res = await axios.post<ITodoResponse>('http://localhost:3333/todos', data)
+    const todo = res.data
+
+    return {
+      _id: todo.id.toString(),
+      name: todo.title,
+      done: todo.completed
+    }
+  }
+
+  return (
+    <div>
+      <h1>{pageTitle}</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" id="title" onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+      
+      {todoList.map(todo => (
+        <Link href={`/todos/${todo._id}`} key={todo._id} passHref>
+          <a style={{ display: "block" }}>{todo.name}</a>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<IHomePageProps> = async (ctx) => {
+  const res = await axios.get<ITodoResponse[]>('http://localhost:3333/todos')
+  const todos = res.data
+
+  return {
+    props: {
+      todos: todos.map(todo => {
+        return {
+          _id: todo.id.toString(),
+          name: todo.title,
+          done: todo.completed
+        }
+      }),
+      pageTitle: "Todos"
+    }
+  }
+}
---
+++ pages/todos/[id].tsx
@@ -0,0 +1,74 @@
+import axios from "axios"
+import { GetStaticPaths, GetStaticProps } from "next"
+
+export type ITodoResponse = {
+  id: number
+  title: string
+  completed: boolean
+}
+
+export type ITodo = {
+  _id: string
+  name: string
+  done: boolean
+}
+
+type ITodoPageProps = {
+  todo: ITodo,
+  pageTitle: string
+}
+
+export default function SingleTodo({ todo, pageTitle }: ITodoPageProps) {
+  return (
+    <main>
+      <h1>{pageTitle}</h1>
+      <div>
+        <small>{todo._id}</small>
+        <h1>{todo.name}</h1>
+        <p>{todo.done ? "Completed" : "Not completed"}</p>
+      </div>
+    </main>
+    
+  )
+}
+
+export const getStaticPaths:GetStaticPaths = async () => {
+  const res = await axios.get<ITodoResponse[]>('https://jsonplaceholder.typicode.com/todos')
+  const todos = res.data
+
+  const paths = todos.map(todo => {
+    return {
+      params: { id: todo.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: "blocking"
+  }
+}
+
+export const getStaticProps:GetStaticProps<ITodoPageProps> = async (ctx) => {
+  try {
+    const { id } = ctx.params
+
+    const res = await axios.get(`http://localhost:3333/todos/${id}`)
+    const todoResponse = res.data
+
+    return {
+      props: {
+        todo: {
+          _id: todoResponse.id.toString(),
+          name: todoResponse.title,
+          done: todoResponse.completed
+        },
+        pageTitle: "Meu todo"
+      },
+      revalidate: 60
+    }
+  } catch (error) {
+    return {
+      notFound: true
+    }
+  }
+}
